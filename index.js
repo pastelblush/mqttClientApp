003a4998a9a478d376a79acf1d058a9f64f7ebed
pastelblush@gmail.com <pastelblush@gmail.com>
2016-03-29 16:43:56 +0800

    dashboard

--- index.js
+++ index.js
@@ -8,61 +8,96 @@ var encoding = require("encoding");
 
 // used npm install encoding and npm install iconv-lite
 
+//chart side
+var t = 1000;
+var x = (new Date()).getTime(); // current time
+var n = 100;
+chart_data = [];
+
+for(i=0; i<n; i++)
+{  
+    chart_data.push([x - (n-1-i)*t,0]);  
+}  
+
 
 var mqtt    = require('mqtt');
 var client  = mqtt.connect('mqtt://localhost');
 // mqtt telemetry part
-function datastruct(){
-  this.boolVal='';
-    this.intVal='';
-    this.dintVal='';
-    this.sintVal='';
-    this.lrealVal='';
-    this.realVal='';
-    this.stringVal='';
+function inputs () {
+     // body...  
+     this.bEmergency = false;
+     this.bIOTerm = false;
+     this.rTemp = 0.0;
+     this.bTemp = false;     
+     this.bFloat1 = false;
+     this.bfloat2 = false;
+     this.bfloat3 = false;
+     this.rTankLevel = 0.0;
+     this.bPump1 = false;
+     this.bPump2 = false;
+     this.bAuto = false;
+     this.rTempLimit = 0.0;
+     // this.syncUpdate = 0;
+   }
 
-} 
 
-
-var obj = new datastruct();
+var obj = new inputs();
 
 
 app.use('/jquery', express.static("node_modules/jquery/dist"));//(__dirname + '\\node_modules\\jquery\\dist\\'));
 app.use(express.static('public'));
 app.use('/jqPlot', express.static("node_modules/jqPlot"));
+app.use('/bootstrap', express.static("node_modules/bootstrap/dist"));
 
 app.get('/',function (req,res) {
    /* body... */
    // res.send('<h1>Hello world<h1>'); 
-   res.sendFile(__dirname + '/index.html');
+   res.sendFile(__dirname + '/dashboard.html');
 });
 
 
 io.on('connection',function (socket) {
    /* body... */ 
    console.log('a user connected')
+   io.emit('plc_input',obj);
+   io.emit('table_data', chart_data);
    socket.on('disconnect',function () {
      /* body... */ 
      console.log('a user disconnected')
    })
 
-   socket.on('chat message', function (msg) {
-     /* body... */ 
-     //socket.broadcast.emit('chat message',msg);
-     io.emit('chat message',msg);
+   // socket.on('chat message', function (msg) {
+   //   /* body... */ 
+   //   //socket.broadcast.emit('chat message',msg);
+   //   io.emit('chat message',msg);
+   // });
+
+   socket.on('bAuto', function (msg) {
+     var jsonstring = JSON.stringify(msg);
+     var resultBuffer = encoding.convert(jsonstring, 'utf8');
+     client.publish('bAuto', resultBuffer);
+     console.log(msg);
    });
 
-   socket.on('bVal', function (msg) {
-     /* body... */ 
-     //socket.broadcast.emit('chat message',msg);
+   socket.on('bPump1', function (msg) {
      var jsonstring = JSON.stringify(msg);
-     //console.log(jsonstring);
-
      var resultBuffer = encoding.convert(jsonstring, 'utf8');
-     console.log(resultBuffer);
-     client.publish('bVal', resultBuffer);
-     //client.publish('triggers', utf8.encode(jsonstring));
-     //console.log(utf8.encode(jsonstring));
+     client.publish('bPump1', resultBuffer);
+     console.log(msg);
+   });
+
+   socket.on('bPump2', function (msg) {
+     var jsonstring = JSON.stringify(msg);
+     var resultBuffer = encoding.convert(jsonstring, 'utf8');
+     client.publish('bPump2', resultBuffer);
+     console.log(msg);
+   });
+
+   socket.on('rTempLimit', function (msg) {
+     var jsonstring = JSON.stringify(msg);
+     var resultBuffer = encoding.convert(jsonstring, 'utf8');
+     client.publish('rTempLimit', resultBuffer);
+     console.log(msg);
    });
 });
 
@@ -76,8 +111,6 @@ http.listen(3000,function () {
 // console.log(obj);
 
 
-
- 
 client.on('connect', function () {
   client.subscribe('presence');
 });
@@ -85,10 +118,23 @@ client.on('connect', function () {
 client.on('message', function (topic, message) {
   // message is Buffer 
   obj = JSON.parse(message);
-  //console.log(obj);
-  //console.log(obj.stringVal)
-
-  io.emit('plc_input',obj)
+  io.emit('plc_input',obj);
 });
+
+
+setInterval(function () {
+   /* body... */ 
+
+  if(chart_data.length > n-1){
+    chart_data.shift();
+  }
+
+  var y = obj.rTankLevel;
+  var x = (new Date()).getTime();
+  chart_data.push([x,y]);
+  io.emit('table_data', chart_data);
+}, 1000)
+
+
 
 console.log(__dirname + '\\node_modules\\jquery\\dist\\');
